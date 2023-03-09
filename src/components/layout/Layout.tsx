733e60604bfe41ea26f2656210095a3b47aa1035
Sebastian Salaberria <s_salaberria@hotmail.com>
2023-03-09 20:34:17 -0300

    added loading while getting session info

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,6 +6,7 @@ import Navbar from './Navbar';
 import pinnedSubeddits from 'src/utils/data/pinned-subeddits.json';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
+import { useSession } from 'next-auth/react';
 
 const DynamicToggle = dynamic(() => Promise.resolve(Toggle), { ssr: false });
 
@@ -21,6 +22,7 @@ const initThemeState = () => {
 
 const Layout = ({ children }: LayoutProps) => {
     const [theme, setTheme] = useState<ThemeOption>(initThemeState);
+    const { status } = useSession();
 
     const toggleTheme = () => {
         const newTheme = theme === 'dark' ? 'light' : 'dark';
@@ -37,28 +39,55 @@ const Layout = ({ children }: LayoutProps) => {
     return (
         <div className={styles.scrollablePage && 'dark:bg-gray-900'}>
             <div className="flex justify-center flex-wrap pt-2 gap-4">
-                {pinnedSubeddits.map(sub => (
-                    <Link href={`/b/${sub}`} key={sub}>
-                        <a>
-                            <p key={sub} className="font-bold clickable">
-                                {sub}
-                            </p>
-                        </a>
-                    </Link>
-                ))}
+                {status !== 'loading' &&
+                    pinnedSubeddits.map(sub => (
+                        <Link href={`/b/${sub}`} key={sub}>
+                            <a>
+                                <p key={sub} className="font-bold clickable">
+                                    {sub}
+                                </p>
+                            </a>
+                        </Link>
+                    ))}
             </div>
             <main className={styles.container}>
-                <div className="absolute right-4 top-10">
-                    <DynamicToggle value={theme} onChange={toggleTheme} />
-                </div>
-                <div className="mb-12">
-                    <Navbar />
-                </div>
-                {children}
+                {status !== 'loading' ? (
+                    <>
+                        <div className="absolute right-4 top-10">
+                            <DynamicToggle
+                                value={theme}
+                                onChange={toggleTheme}
+                            />
+                        </div>
+                        <div className="mb-12">
+                            <Navbar />
+                        </div>
+                        {children}{' '}
+                    </>
+                ) : (
+                    <div className="flex h-full justify-center my-auto text-[6rem] lg:text-[5rem] md:text-[5rem] font-extrabold">
+                        <h2 className="relative">
+                            {'Beddit'.split('').map((char, index) => (
+                                <span
+                                    className={`animated-text ${
+                                        index > 2 && 'text-purple-300'
+                                    }`}
+                                    style={{
+                                        animationDelay: `${0.3 + index / 10}s`,
+                                    }}
+                                >
+                                    {char}
+                                </span>
+                            ))}
+                        </h2>
+                    </div>
+                )}
             </main>
-            <footer className="flex justify-center h-60 ">
-                Made with ❤️ by Sebastián Salaberría
-            </footer>
+            {status !== 'loading' && (
+                <footer className="flex justify-center h-60 ">
+                    Made with ❤️ by Sebastián Salaberría
+                </footer>
+            )}
         </div>
     );
 };
